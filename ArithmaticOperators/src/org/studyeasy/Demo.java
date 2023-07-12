a151e31210b035bfdc04072a282fe7018c2337b3
rohini-kshirsagar <rohinikshirsagar0609@gmail.com>
2023-07-12 12:21:01 +0530

    Add the new code

---
+++ ArithmaticOperators/src/org/studyeasy/Demo.java
@@ -0,0 +1,13 @@
+package org.studyeasy;
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		int x = 1;
+		int y = 12*2;
+		int z = 13 % 2;
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
